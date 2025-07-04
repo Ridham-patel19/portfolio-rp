dd2159072f23b03cf14f2b5ee0fd5b93b72917ea
Ridham-patel19 <patelridham737@gmail.com>
2025-07-04 14:34:00 +0530

    changed for deplyment

--- backend/app.js
+++ backend/app.js
@@ -42,6 +42,7 @@ app.use((req, res) => {
     res.status(404).render('404')
 })
 
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
     console.log('hello from the server')
 })
